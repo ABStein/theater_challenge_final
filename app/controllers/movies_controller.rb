721ddd2d88cf17ad3f296f58986d97eb88ec7a72
ABStein <abs19stein@gmail.com>
2018-10-31 19:39:29 -0500

    installed bootstrap, added some styling to the page with a background, made a crud for movies with an index page only

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,53 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+
+  end
+
+  def create
+    movie = Movie.new(
+                     title: params[:title],
+                     rating: params[:rating],
+                     runtime: params[:runtime],
+                     img: params[:img],
+                     showtimes: params[:showtimes]
+                     )
+
+     movie.save
+     flash[:success] = "Movie successfully Created"
+     redirect_to '/'
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+    @movie.assign_attributes(
+                            title: params[:title],
+                            rating: params[:rating],
+                            runtime: params[:runtime],
+                            img: params[:img],
+                            showtimes: params[:showtimes]
+                            )
+
+    @movie.save
+    flash[:success] = "Movie successfully updated"
+    redirect_to "/"
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:warning] = "Movie Deleted"
+    redirect_to "/"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #                   img: "https://dtvimages.hs.llnwd.net/e1//db_photos/movies/AllPhotosAPGI/34483/34483_aa.jpg",
 #                   showtimes: [{"12:30 PM" => 20}, {"4:30 PM" => 15}, {"8:15 PM" => 8}, {"10:00 PM" => 5}]
 #                   )
+
+# movie.save
